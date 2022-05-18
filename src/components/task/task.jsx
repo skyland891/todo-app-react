1a7c4979eba5370267677b782b87eeee2b7cd7d2
Ruslan <uruslan2000@gmail.com>
2022-05-18 13:20:41 +0500

    Изменения статуса задачи + удаление задачи

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -1,17 +1,54 @@
 import React from 'react'
 
-function Task({label, distance}) {
+
+function Task({id, label, distance, changeStatus, deleteTask}) {
+
+  const toggleCheck = (checked) => {
+    changeStatus(checked ? 'completed' : 'active', id);
+  }
+
   return (
     <div className="view">
-        <input className="toggle" type="checkbox"/>
+        <input className="toggle" type="checkbox" onChange={(e) => {
+          toggleCheck(e.target.checked);
+        }}/>
         <label>
           <span className="description">{label}</span>
           <span className="created">{distance}</span>
         </label>
         <button className="icon icon-edit"></button>
-        <button className="icon icon-destroy"></button>
+        <button className="icon icon-destroy" onClick={() => {deleteTask(id)}}></button>
     </div>
   )
 }
 
-export default Task
+/*
+export default class Task extends React.Component {
+  constructor(){
+    super();
+  }
+
+  toggleCheck = (status) => {
+
+  }
+
+  render(){
+    const {label, distance} = this.props;
+
+    return (
+      <div className="view">
+          <input className="toggle" type="checkbox" onChange={(e) => {
+            toggleCheck(e.target.checked);
+          }}/>
+          <label>
+            <span className="description">{label}</span>
+            <span className="created">{distance}</span>
+          </label>
+          <button className="icon icon-edit"></button>
+          <button className="icon icon-destroy"></button>
+      </div>
+    )
+  }
+}
+*/
+export default Task;
