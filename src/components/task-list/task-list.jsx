bb4c672539b584d5eca723342b26778e9209b43a
Ruslan <uruslan2000@gmail.com>
2022-06-19 15:33:22 +0500

    Переписка приложения на хуках

--- src/components/task-list/task-list.jsx
+++ src/components/task-list/task-list.jsx
@@ -1,116 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Task from "../task";
 import "./task-list.css";
-/*
-function TaskList({tasks, changeStatus, deleteTask}) {
+
+function TaskList({
+  tasks,
+  editDescription,
+  changeStatus,
+  startTimer,
+  stopTimer,
+  deleteTask,
+}) {
+  const [inputValue, setInputValue] = useState("");
+  const [inputId, setInputId] = useState(null);
+  const [prevStatus, setPrevStatus] = useState(null);
+
+  const onChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    editDescription(inputValue, inputId, prevStatus);
+    setInputValue("");
+    setInputId(null);
+    setPrevStatus(null);
+  };
+
+  const editClick = (taskName, id, status) => {
+    if (inputId) {
+      editDescription(inputValue, inputId, prevStatus);
+    }
+    setInputValue(taskName);
+    setInputId(id);
+    setPrevStatus(status);
+    changeStatus("editing", id);
+  };
+
   return (
     <ul className="todo-list">
-      {tasks.map(task => (
-        <li key={task.id} className= {task.status}>
-          <Task id= {task.id} label= {task.description} distance= {task.createdDistance} status= {task.status} changeStatus= {changeStatus} deleteTask= {deleteTask}/>
-          <input type="text" className= {task.status === 'editing' ? 'edit' : 'hidden'} value="Editing task"/>
+      {tasks.map((task) => (
+        <li key={task.id} className={task.status}>
+          <Task
+            id={task.id}
+            label={task.description}
+            timerMin={task.timerMin}
+            timerSec={task.timerSec}
+            distance={task.createdDistance}
+            status={task.status}
+            changeStatus={changeStatus}
+            deleteTask={deleteTask}
+            editClick={editClick}
+            startTimer={startTimer}
+            stopTimer={stopTimer}
+          />
+
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              className={task.status === "editing" ? "edit" : "hidden"}
+              placeholder="Editing task"
+              value={inputValue}
+              onChange={onChange}
+            />
+          </form>
         </li>
       ))}
     </ul>
-  )
+  );
 }
-*/
 
-class TaskList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      inputValue: "",
-      inputId: null,
-      prevStatus: null,
-    };
-  }
+TaskList.defaultProps = {
+  startTimer: () => {},
+  stopTimer: () => {},
+  editDescription: () => {},
+  changeStatus: () => {},
+  deleteTask: () => {},
+  tasks: [],
+};
 
-  onChange = (e) => {
-    this.setState({
-      inputValue: e.target.value,
-    });
-  };
-
-  onSubmit = (e) => {
-    e.preventDefault();
-    const { inputValue, inputId, prevStatus } = this.state;
-    this.props.editDescription(inputValue, inputId, prevStatus);
-    this.setState({
-      inputValue: "",
-      inputId: null,
-      prevStatus: null,
-    });
-  };
-
-  static defaultProps = {
-    startTimer: () => {},
-    stopTimer: () => {},
-    editDescription: () => {},
-    changeStatus: () => {},
-    deleteTask: () => {},
-    editClick: () => {},
-  };
-
-  static propTypes = {
-    editDescription: PropTypes.func.isRequired,
-    startTimer: PropTypes.func.isRequired,
-    stopTimer: PropTypes.func.isRequired,
-    changeStatus: PropTypes.func.isRequired,
-    deleteTask: PropTypes.func.isRequired,
-    editClick: PropTypes.func.isRequired,
-    tasks: PropTypes.arrayOf(PropTypes.object),
-  };
-
-  editClick = (taskName, id, status) => {
-    if (this.state.inputId) {
-      const { inputValue, inputId, prevStatus } = this.state;
-      this.props.editDescription(inputValue, inputId, prevStatus);
-    }
-    this.setState({
-      inputValue: taskName,
-      inputId: id,
-      prevStatus: status,
-    });
-    this.props.changeStatus("editing", id);
-  };
-
-  render() {
-    const { tasks, changeStatus, deleteTask, startTimer, stopTimer } =
-      this.props;
-    return (
-      <ul className="todo-list">
-        {tasks.map((task) => (
-          <li key={task.id} className={task.status}>
-            <Task
-              id={task.id}
-              label={task.description}
-              timerMin={task.timerMin}
-              timerSec={task.timerSec}
-              distance={task.createdDistance}
-              status={task.status}
-              changeStatus={changeStatus}
-              deleteTask={deleteTask}
-              editClick={this.editClick}
-              startTimer={startTimer}
-              stopTimer={stopTimer}
-            />
-
-            <form onSubmit={this.onSubmit}>
-              <input
-                type="text"
-                className={task.status === "editing" ? "edit" : "hidden"}
-                placeholder="Editing task"
-                value={this.state.inputValue}
-                onChange={this.onChange}
-              />
-            </form>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+TaskList.propTypes = {
+  editDescription: PropTypes.func.isRequired,
+  startTimer: PropTypes.func.isRequired,
+  stopTimer: PropTypes.func.isRequired,
+  changeStatus: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
+  tasks: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default TaskList;
--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -1,153 +1,114 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import PropTypes from "prop-types";
-/*
-function Task({id, label, distance, status, changeStatus, deleteTask, editClick}) {
+
+function Task({
+  id,
+  label,
+  status,
+  changeStatus,
+  deleteTask,
+  distance,
+  editClick,
+  stopTimer,
+  startTimer,
+  timerMin,
+  timerSec,
+}) {
+  const [formatDistance, setFormatDistance] = useState("");
+  const [createdIntervalId, setCreatedIntervalId] = useState(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setFormatDistance(
+        formatDistanceToNow(new Date(distance), {
+          includeSeconds: true,
+          addSuffix: true,
+        })
+      );
+      setCreatedIntervalId(intervalId);
+    }, 1000);
+    return () => {
+      if (createdIntervalId) {
+        clearInterval(createdIntervalId);
+        setCreatedIntervalId(null);
+      }
+    };
+  }, []);
 
   const toggleCheck = (checked) => {
-    changeStatus(checked ? 'completed' : 'active', id);
-  }
+    changeStatus(checked ? "completed" : "active", id);
+    if (checked) {
+      stopTimer(id);
+    }
+  };
 
   return (
     <div className="view">
-
-        <input 
-        className= "toggle" 
-        checked= {status === 'completed'} 
-        type="checkbox" 
-        onChange= {(e) => {
+      <input
+        className="toggle"
+        checked={status === "completed"}
+        type="checkbox"
+        onChange={(e) => {
           toggleCheck(e.target.checked);
-        }}/>
+        }}
+      />
 
-        <label>
-          <span className="description">{label}</span>
-          <span className="created">{formatDistanceToNow(new Date(distance), {includeSeconds: true, addSuffix: true})}</span>
-        </label>
-        <button className="icon icon-edit" onClick={() => {
+      <label>
+        <span className="title">{label}</span>
+        <span className="description">
+          <button
+            className="icon icon-play"
+            onClick={() => {
+              startTimer(id);
+            }}
+          ></button>
+          <button
+            className="icon icon-pause"
+            onClick={() => {
+              stopTimer(id);
+            }}
+          ></button>
+          &nbsp;&nbsp;{`${timerMin}:${timerSec}`}
+        </span>
+        <span className="description">{formatDistance}</span>
+      </label>
+      <button
+        className="icon icon-edit"
+        onClick={() => {
           editClick(label, id, status);
-        }}></button>
-        <button className="icon icon-destroy" onClick={() => {deleteTask(id)}}></button>
+        }}
+      ></button>
+      <button
+        className="icon icon-destroy"
+        onClick={() => {
+          deleteTask(id);
+        }}
+      ></button>
     </div>
-  )
+  );
 }
-*/
-class Task extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      distance: "",
-      createdIntervalId: null,
-    };
-  }
 
-  static defaultProps = {
-    changeStatus: () => {},
-    deleteTask: () => {},
-    editClick: () => {},
-    startTimer: () => {},
-    stopTimer: () => {},
-  };
+Task.propTypes = {
+  id: PropTypes.number.isRequired,
+  label: PropTypes.string.isRequired,
+  status: PropTypes.oneOf(["completed", "active", "editing"]).isRequired,
+  changeStatus: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
+  distance: PropTypes.string.isRequired,
+  editClick: PropTypes.func.isRequired,
+  timerMin: PropTypes.string.isRequired,
+  timerSec: PropTypes.string.isRequired,
+  startTimer: PropTypes.func.isRequired,
+  stopTimer: PropTypes.func.isRequired,
+};
 
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-    label: PropTypes.string.isRequired,
-    status: PropTypes.oneOf(["completed", "active", "editing"]).isRequired,
-    changeStatus: PropTypes.func.isRequired,
-    deleteTask: PropTypes.func.isRequired,
-    editClick: PropTypes.func.isRequired,
-    distance: PropTypes.string.isRequired,
-    timerMin: PropTypes.string.isRequired,
-    timerSec: PropTypes.string.isRequired,
-    startTimer: PropTypes.func.isRequired,
-    stopTimer: PropTypes.func.isRequired,
-  };
-
-  componentDidMount() {
-    const createdIntervalId = setInterval(() => {
-      this.setState({
-        distance: formatDistanceToNow(new Date(this.props.distance), {
-          includeSeconds: true,
-          addSuffix: true,
-        }),
-        createdIntervalId: createdIntervalId,
-      });
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    if (this.state.createdIntervalId) {
-      clearInterval(this.state.createdIntervalId);
-      this.setState({
-        createdIntervalId: null,
-      });
-    }
-  }
-
-  render() {
-    const {
-      id,
-      label,
-      status,
-      changeStatus,
-      deleteTask,
-      editClick,
-      stopTimer,
-      startTimer,
-      timerMin,
-      timerSec,
-    } = this.props;
-
-    const toggleCheck = (checked) => {
-      changeStatus(checked ? "completed" : "active", id);
-      if (checked) {
-        stopTimer(id);
-      }
-    };
-    return (
-      <div className="view">
-        <input
-          className="toggle"
-          checked={status === "completed"}
-          type="checkbox"
-          onChange={(e) => {
-            toggleCheck(e.target.checked);
-          }}
-        />
-
-        <label>
-          <span className="title">{label}</span>
-          <span className="description">
-            <button
-              className="icon icon-play"
-              onClick={() => {
-                startTimer(id);
-              }}
-            ></button>
-            <button
-              className="icon icon-pause"
-              onClick={() => {
-                stopTimer(id);
-              }}
-            ></button>
-            &nbsp;&nbsp;{`${timerMin}:${timerSec}`}
-          </span>
-          <span className="description">{this.state.distance}</span>
-        </label>
-        <button
-          className="icon icon-edit"
-          onClick={() => {
-            editClick(label, id, status);
-          }}
-        ></button>
-        <button
-          className="icon icon-destroy"
-          onClick={() => {
-            deleteTask(id);
-          }}
-        ></button>
-      </div>
-    );
-  }
-}
+Task.defaultProps = {
+  changeStatus: () => {},
+  deleteTask: () => {},
+  editClick: () => {},
+  startTimer: () => {},
+  stopTimer: () => {},
+};
 
 export default Task;
